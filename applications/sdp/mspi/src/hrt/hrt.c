27ca1d68a7b0fe12fa58362721060cc9412e522a
Magdalena Pastula <magdalena.pastula@nordicsemi.no>
2025-01-22 19:26:12 +0100

    Experiments with working RX in shift 24
    Signed-off-by: Magdalena Pastula <magdalena.pastula@nordicsemi.no>

--- applications/sdp/mspi/src/hrt/hrt.c
+++ applications/sdp/mspi/src/hrt/hrt.c
@@ -250,23 +250,14 @@ void hrt_read(volatile hrt_xfer_t *hrt_xfer_params)
 
 	// nrf_vpr_csr_vtim_simple_wait_set(0, false, 0);
 
-	// nrf_vpr_csr_vio_shift_ctrl_t in_shift_ctrl = {
-	// 	.shift_count = 7,
-	// 	.out_mode = NRF_VPR_CSR_VIO_SHIFT_OUTB_TOGGLE,
-	// 	.frame_width = 4,
-	// 	.in_mode = NRF_VPR_CSR_VIO_MODE_IN_SHIFT,
-	// };
-
-	// nrf_vpr_csr_vio_shift_ctrl_buffered_set(&shift_ctrl);
-
 	// nrf_vpr_csr_vio_out_buffered_set(nrf_vpr_csr_vio_out_get());
 	// nrf_vpr_csr_vtim_simple_wait_set(0, false, 0);
 
-	hrt_xfer_params->xfer_data[HRT_FE_DATA].vio_inb_get();
+	// hrt_xfer_params->xfer_data[HRT_FE_DATA].vio_inb_get();
 
-	nrf_vpr_csr_vio_shift_ctrl_buffered_set(&in_shift_ctrl);
+	// nrf_vpr_csr_vio_shift_ctrl_buffered_set(&in_shift_ctrl);
 
-	for (uint8_t i = 0; i < hrt_xfer_params->xfer_data[HRT_FE_DATA].words; i++)
+	for (uint8_t i = 0; i < hrt_xfer_params->xfer_data[HRT_FE_DATA].words + 1; i++)
 	{
 		hrt_xfer_params->xfer_data[HRT_FE_DATA].rx_data[i] = hrt_xfer_params->xfer_data[HRT_FE_DATA].vio_inb_get() >> 8;
 	}
